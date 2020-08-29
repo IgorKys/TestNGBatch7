2eb2f4d537e6575c45ba6f58dc9b549d6f8a96e8
IgorKysil <igork2210@gmail.com>
2020-08-29 08:48:53 -0700

    TestNg

--- src/com/syntax/class01/TestNGIntro.java
+++ src/com/syntax/class01/TestNGIntro.java
@@ -4,19 +4,28 @@ import org.testng.annotations.Test;
 
 public class TestNGIntro {
 
-	@Test
+	// @Test is a main annotation that identifies how many tests we will be running
+	
+	@Test // we use this annotation to mark a method as a test method
 	public void testOne() {
 		System.out.println("I am test 1");
 	}
-	
+
 	@Test
 	public void testTwo() {
 		System.out.println("I am test 2");
 	}
-	
+
 	@Test
 	public void testThree() {
 		System.out.println("I am test 3");
 	}
+
+	@Test
+	public void testFour() {
+		System.out.println("I am test 4");
+	}
+
+	// all test methods will get executed in alphabetical order
 
 }
